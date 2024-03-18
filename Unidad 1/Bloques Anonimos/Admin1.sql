19f2696f6e73358150769076ff1a49f56b8a230f
JoseDiazGonzalez <joseantonio.diazg@hotmail.com>
2024-03-17 22:54:53 -0300

    Two commit

---
+++ Unidad 1/Bloques Anonimos/Admin1.sql
@@ -0,0 +1,16 @@
+DECLARE
+    v_nombre VARCHAR(20);
+    v_apellido Varchar(20);
+BEGIN
+    SELECT NOMBRE_CLI, appaterno_cli
+    INTO v_nombre, v_apellido
+    FROM CLIENTE
+    WHERE numrut_cli = 4604866;
+
+
+
+    DBMS_OUTPUT.PUT_LINE('El nombre del clinete es '|| v_nombre||' '|| v_apellido );
+    
+End;
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
